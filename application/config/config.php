7f82802ae0e35728440e2c90450074c84894dc83
gagendra Ghalley <0216506.cst@rub.edu.bt>
2020-03-15 00:10:02 +0600

    alumni_management_system

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/app/las04';
+$config['base_url'] = 'http://localhost:8080/app/alumni_mangement_system';
 $config['server_root'] =$_SERVER['DOCUMENT_ROOT'];
 
 /*
