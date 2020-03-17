d0e4e288bbd0475551092c29991716c83cbeb41b
lekiyangdon <lekiyangdon@gmail.com>
2020-03-17 23:21:27 +0600

    Join us
    There was duplication for Join us button

--- application/views/alumni1.php
+++ application/views/alumni1.php
@@ -270,7 +270,7 @@
                         <div class="badge"  class="bg-primary">
                              <span> <button style="height:70px;width:75px;font-size:15px;" class="btn btn-primary btn-lg" data-animation="animated zoomInUp">
 
-                                    <a href="<?php echo base_url();?>index.php/ATD/login1/"><font color="white" >Join Us</font> </a>
+                                    <a href="<?php echo base_url();?>index.php/ATD/login1/"><font color="white" >Join Now</font> </a>
 
                                 </button></span>
 
