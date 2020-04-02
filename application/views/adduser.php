ffe0b9749f88816291b2a96aff23850cacd4d777
Gagendra-Ghalley <35451725+Gagendra-Ghalley@users.noreply.github.com>
2020-04-02 13:24:43 +0600

    added new view
    add admin profile
    delete event
    delete user
    view detail of admni

---
+++ application/views/adduser.php
@@ -0,0 +1,78 @@
+
+        <!--  page-wrapper -->
+        <div id="page-wrapper">
+
+            <div class="row">
+                <!-- Page Header -->
+                <div class="col-lg-12">
+                    <h1 class="page-header"></h1> <div class="clockwrapper"></div>
+                </div>
+                <!--End Page Header -->
+            </div>
+
+        
+
+
+            <div class="row">
+                <div class="col-lg-12">
+
+
+
+                  
+                    <!--Simple table example -->
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <i class="fa fa-bar-chart-o fa-fw"></i> <span>Add Event</span>
+                            <div class="pull-right">
+                               
+                            </div>
+                        </div>
+
+                        
+            <div class="col-md-8 col-md-offset-2">
+                <div class="login-panel panel panel-default">                  
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Add user</h3>
+                    </div>
+
+
+          <div class="panel-body login">
+                        <form class="form-group">
+                            
+                              <div class="form-group">
+                               
+                                    <input class="form-control" placeholder="Enter user cid number" name="cid" type="text"/>
+                                </div>
+                              
+                                
+                                <input type="submit" value="Add" class="btn btn-lg btn-success btn-block"/>
+                                <br>
+
+                           
+                        </form>
+                       
+                    </div>
+                    </div>
+                </div>
+                      
+                    </div>
+                    
+
+                   
+
+  
+
+           
+
+                      
+
+
+         
+
+
+        </div>
+     
+
+    </div>
+  </div>
+    <!-- end wrapper -->
---
+++ application/views/admin_profile.php
@@ -0,0 +1,145 @@
+
+        <!--  page-wrapper -->
+        <div id="page-wrapper">
+
+            <div class="row">
+                <!-- Page Header -->
+                <div class="col-lg-12">
+                    <h1 class="page-header"></h1> <div class="clockwrapper"></div>
+                </div>
+                <!--End Page Header -->
+            </div>
+
+        
+
+
+            <div class="row">
+                <div class="col-lg-12">
+
+
+
+                  
+                    <!--Simple table example -->
+                    <div class="panel panel-primary">
+                        
+                            <ol class="breadcrumb bg-primary ">
+                                <li class="active"><a href="#"><h5 style="color: #fff">Edit Information</h5></a></li>
+                                <li><a href="<?php echo base_url()?>index.php/Leave/view_detail/" style="color: #fff">View Details</a> </li>
+                            </ol>
+                      
+                           
+                      
+                        <div class="panel-body">
+                            <div class="row">
+                               <div class="col-md-12">
+                                <div class="row du-contact-form"> 
+                                <div class="team-img">
+                                    <div class="col-md-2 img-thumbnail">
+                                <img src="<?php echo base_url();?>assets/img2/1tlhendup.jpg" style="height:150px;width: 150px;border-radius: 70%"/> 
+                                    </div>
+                                   <div class="col-md-9">
+                                   <br><br><br>      
+                                       <button class="btn btn-primary"><input type="file" name="userfile" size="24" /></button>
+                                            <br />
+                                            <p style="color: #111">Acceptable formats jpg and png only <br>
+                                            Max file size is 500 kb and min 70 kb</p>
+
+                                            <input type="submit" value="upload" /><br>
+                                          
+                            
+                                   </div>
+                                   
+                                  
+                                    </div>
+                                 </div> 
+                                  <hr>
+                              </div>
+
+                            
+                         
+                             <div class="col-md-12">
+                                        <h3>ACCOUNT INFORMATION</h3><br>
+                                        <div class="col-md-2">
+                                            <h5><b>First Name</b></h5>
+                                        </div>
+                                         <div class="form-group col-md-10"> 
+                                      
+                                      <input type="text" class="form-control" placeholder="First Name..."> 
+                                    </div> 
+
+                                    <div class="col-md-2">
+                                            <h5><b>Last Name</b></h5>
+                                        </div>
+                                         <div class="form-group col-md-10"> 
+                                      
+                                      <input type="text" class="form-control" placeholder="Last Name..."> 
+                                    </div>
+
+                                    <div class="col-md-2">
+                                            <h5><b>Email</b></h5>
+                                        </div>
+                                         <div class="form-group col-md-10"> 
+                                      
+                                      <input type="text" class="form-control" placeholder="Email..."> 
+                                    </div><br>
+                                          <button class="btn btn-link text-primary btn-lg" style="margin-left:900px"><b><a href="#">Update </a></b></button>
+
+                                    
+                                    
+                                   </div>
+                                 
+                                  
+
+                                   <div class="col-md-12">
+                                     <hr>
+                                       <h3>CHANGE PASSWORD</h3><br>
+                                       <div class="col-md-2">
+                                            <h5><b>Current Password<b/></h5>
+                                        </div>
+                                         <div class="form-group col-md-10"> 
+                                      
+                                      <input type="Password" class="form-control" placeholder="Current Password..."> 
+                                    </div>
+
+                                     <div class="col-md-2">
+                                            <h5><b>New Password<b/></h5>
+                                        </div>
+                                         <div class="form-group col-md-10"> 
+                                      
+                                      <input type="Password" class="form-control" placeholder="New Password..."> 
+                                    </div>
+
+                                     <div class="col-md-2">
+                                            <h5><b>Confirm Password<b/></h5>
+                                        </div>
+                                         <div class="form-group col-md-10"> 
+                                      
+                                      <input type="Password" class="form-control" placeholder="Confirm Password..."> 
+                                    </div>
+                                    <button class="btn btn-link text-primary btn-lg" style="margin-left:900px"><b><a href="#">Update </a></b></button>
+                                   </div>
+                           
+                        </div>
+
+                      
+                    </div>
+                    
+
+                     </div> 
+
+  
+
+           
+
+                      
+
+
+         
+
+
+        </div>
+     
+
+    </div>
+  </div>
+    <!-- end wrapper -->
