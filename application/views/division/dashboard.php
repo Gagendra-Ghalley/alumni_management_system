24c76c5d6be1749e10956e41b8de58c3ab2f84c3
lekiyangdon <lekiyangdon@gmail.com>
2020-04-05 19:14:14 +0600

    event dashboard

--- application/views/division/dashboard.php
+++ application/views/division/dashboard.php
@@ -51,27 +51,6 @@
                   
                 </div>
             </div>
-
-
-
-
-
-
-            <div class="col-md-2 col-sm-6 " >
-<?php foreach($user->result() as $row){?>
-        
-       
-        
-              <form method="post" accept-charset="utf-8" action="<?php echo base_url();?>index.php/Settings/event_validate"   class="form-group">
-                <input type="hidden" name="cid" value="<?php echo $row->cid;?>">
-                <input type="submit" value="Join Us" style="height:37px;width:95px;font-size:15px;" class="btn btn-primary btn-lg" data-animation="animated zoomInUp">
-                </form> 
-                 <?php } ?>
-           
-    <table>
-      <tbody>
-
-
             <div class="col-md-2 col-sm-6 " >
                 
 
@@ -114,47 +93,15 @@
                    
 
                             <table>
-                             <tbody>
-
+                            <!--  <tbody>
                                                <?php $count1=0; foreach($request1 as $row):?>
                                                
                                                 <tr>
                                                   <?php  $count1++;?>
-
-                                                </tr> 
-                                                <?php endforeach;?>
-                                            </tbody>
-                                        </table> 
-                                        <button class="btn btn-default" style="height:70px;width:100px;font-size:15px;font-family:georgia;text-shadow: 1px 1px 1px #111">  
-                                        <p><font color="black"><?php
-                                        
-                                        echo ($count1);
-                                        echo (" Members <br> Interested");?></font></p>
-                                          </button><br><br>
-                
-            </div>
-        </div>
- <div class="row">
-                <div class="col-md-5 col-sm-12">
-                    <p class="text-primary"><b>Description</b></p>
-                </div>
-               
-            </div>
-             <div class="container">
-                <div class="col-md-10 col-sm-12">
-                    
-                        
-                        <p class="text-bold text-center text-justify" style="font-family:georgia; color:black; font-size: 15px">Faculty Development program cum workshop on climate Across the curriculum. Educational Resource for Teacher's organized by center for Disater Risk Reducation<span id="dots">...</span> <span id="more"> and community development studies (CDRR & CDS) college pf science and Technology, Royal University of Bhutan and Commission of Education and outreach, international association of seismology.Support student abd our community Even a small gift can make a big impact on generations so come and change lives. Give to CST College, awhether you just graduated and started new job or contiue with your higher education, your alumni association is here for you. Let us be a partner in your success. ISupport student abd our community, Even a small gift can make a big impact on generations so come and change lives. Give to CST College.upport student and our community Even a small gift can make a big impact on generations so come and change lives. Give to CST College.upport student and our community Even a small gift can make a big impact on generations so come and change lives. Give to CST College.upport student and our community Even a small gift can make a big impact on generations so come and change lives. Give to CST College</span></p>
-                        
-                        <div class="col-md-10">
-                        <button class="btn btn-link text-primary" onclick="myFunction()" id="myBtn"><b>Read more</b></button>
-                        </div>
-
-
                                                 </tr>     
                                                
-                                               
-                                            </tbody> 
+                                                <?php endforeach;?>
+                                            </tbody> -->
                                         </table> 
                                    <button class="btn btn-default" style="height:70px;width:100px;font-size:15px;font-family:georgia;text-shadow: 1px 1px 1px #111">  
                                         <p><font color="black"><?php
@@ -171,7 +118,7 @@
                
             </div>
         </div>
-
+ <div class="container">
                 
              <div class="container">
                 <div class="col-md-10 col-sm-12">
@@ -194,27 +141,19 @@
                         
                     </div>
                         </div></div></div>  <?php endforeach; ?> 
-
-                     
-
-
                       <!--   <p class="text-bold text-center text-justify" style="font-family:georgia; color:black; font-size: 15px">Faculty Development program cum workshop on climate Across the curriculum. Educational Resource for Teacher's organized by center for Disater Risk Reducation<span id="dots">...</span> <span id="more"> and community development studies (CDRR & CDS) college of science and Technology, Royal University of Bhutan and Commission of Education and outreach, international association of seismology.Support student abd our community Even a small gift can make a big impact on generations so come and change lives. Give to CST College, awhether you just graduated and started new job or contiue with your higher education, your alumni association is here for you. Let us be a partner in your success. ISupport student abd our community, Even a small gift can make a big impact on generations so come and change lives. Give to CST College.upport student and our community Even a small gift can make a big impact on generations so come and change lives. Give to CST College.upport student and our community Even a small gift can make a big impact on generations so come and change lives. Give to CST College.upport student and our community Even a small gift can make a big impact on generations so come and change lives. Give to CST College</span></p> -->
                         
                        <!--  <div class="col-md-10">
                         <button class="btn btn-link text-primary" onclick="myFunction()" id="myBtn"><b>Read more</b></button>
-                        </div> -->                        
-
+                        </div> -->
+                        
             </div>
         </div>
         
- 
+    </div>   
 
               
  </div>
-
-
- </div>
-</div>
 
 
       <div class="bg-primary" >
