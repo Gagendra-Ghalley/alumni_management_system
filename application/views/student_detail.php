52fa0ec16a2735884fb52f36b9fb3bd1423b8b55
Gagendra-Ghalley <35451725+Gagendra-Ghalley@users.noreply.github.com>
2020-04-06 13:28:23 +0600

    admin view editting

---
+++ application/views/student_detail.php
@@ -0,0 +1,126 @@
+
+<html>  
+ <head>  
+    <title>Insert</title>   
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />  
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
+
+   
+   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />   -->
+   <link href="<?php echo base_url();?>assets/plugins/bootstrap/bootstrap.css" rel="stylesheet" />
+    <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="<?php echo base_url();?>assets/plugins/pace/pace-theme-big-counter.css" rel="stylesheet" />
+   
+    <link href="<?php echo base_url();?>assets/css/main-style.css" rel="stylesheet" />
+   
+    <!-- Page-Level CSS -->
+    <link href="<?php echo base_url();?>assets/plugins/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <link href="<?php echo base_url();?>assets/css/cst6.css" rel="stylesheet"/>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
+
+ </head>  
+  
+ 
+ 
+ <body>  
+         <div id="page-wrapper">
+
+          
+          <div class="row profile">
+            
+
+            
+
+     
+      <h2>Student Details</h2><br />
+      <!-- <div class="panel-body"> -->
+      <div class="table-responsive">
+        <div class="col-lg-10">
+         
+ <table class="table table-bordered ">
+         <div class="row">
+                <div class="col-lg-12">
+
+  
+ <div class="panel panel-primary">
+                        <div class="panel-heading">
+                          
+                            <i class="fa fa-bar-chart-o fa-fw"></i>Users
+                           
+                            <div class="pull-right">
+      
+      
+                <tr>  
+                     <th >Sl.no</th>  
+                     <th >Name</th>  
+                     <th >CID</th>  
+                     <th >Department</th>
+                     <th >Email</th>
+                     <th >Year of Graduation</th>  
+                      
+                </tr>
+            <?php $counter=1; ?>  
+           <?php  
+           if($fetch_data->num_rows() > 0)  
+           {  
+
+                foreach($fetch_data->result() as $row)  
+                {  
+           ?>  
+
+          
+                <tr> <td><?php echo $counter;   ?></td> 
+                     
+                     <td style="width: 150px;"><?php echo $row->name; ?></td> 
+                     <td style="width: 150px;"><?php echo $row->cid; ?></td> 
+                     <td style="width: 150px;"><?php echo $row->ParentAgency; ?></td>
+                     <td style="width: 150px;"><?php echo $row->Email; ?></td>
+                     <td style="width: 150px;"><?php echo $row->Agency; ?></td> 
+                    <!--  <td style="width: 150px;"><?php echo $row->year; ?></td> --> 
+                     <!--  <td style="width: 150px;"><?php echo $row->course; ?></td> 
+                     <td style="width: 150px;"><?php echo $row->year; ?></td>   
+                     <td style="width: 150px;"><a href="#" class="delete_data" id="<?php echo $row->cid; ?>">Delete</a></td>  --> 
+                     
+                </tr> 
+                <?php $counter++; ?> 
+           <?php       
+                }  
+           }  
+           else  
+           {  
+           ?>  
+                <tr>  
+                     <td colspan="5">No Data Found</td>  
+                </tr>  
+           <?php  
+           }  
+           ?>  
+           </table>  
+      </div>  
+    </div>
+  </div>
+</div>
+ <div class="bg-primary" class="du-newsletter">
+      <div class="bg-primary" class="du-newsletter" >
+            <div class="container">
+
+               
+
+
+                <div class="col-lg-12">
+
+                <div class="col-md-6 col-sm-12">
+
+
+                    <p>@Alumni Management System</p>
+
+                </div>
+
+            </div>
+        </div>
+      </div>
+    </div>
+  
+
+ </body>  
+ </html>  
--- application/views/superadmin/edit_event.php
+++ application/views/superadmin/edit_event.php
@@ -110,18 +110,14 @@
                                         echo (" Members <br> Interested");?></font></p>
                                           </button><br><br>
 
-                                <button style="height:70px;width:95px;font-size:15px;" class="btn btn-primary btn-lg" data-animation="animated zoomInUp">
-
-                                    <a href="<?php echo base_url();?>index.php/ATD/login1/"><font color="white" >Join Us</font> </a>
-
-                                </button>       
+                                    
                
             </div>
         </div>
- <div class="container">
+ 
                
-             <div class="container">
-                <div class="col-md-10 col-sm-12">
+           
+  <div class="col-md-10 col-sm-12">
 
      <br>
                         <?php foreach($eventdetail as $row): ?>
@@ -205,6 +201,7 @@
             </div>
         </div>
       </div>
+    </div>
 
  <script>
 
