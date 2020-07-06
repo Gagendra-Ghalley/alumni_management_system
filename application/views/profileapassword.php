de43facd6fb67aaa2986c1d9d0af1fe4af55b7f1
Nima Wangchuk Tamang <53468613+nimawt@users.noreply.github.com>
2020-07-06 22:55:31 +0530

    changes
    changes

--- application/views/profileapassword.php
+++ application/views/profileapassword.php
@@ -62,7 +62,7 @@
             <div class="container">
                 <div class="col-lg-12">
                 <div class="col-md-6 col-sm-12">
-                    <p>Alumni Management System@2020</p>
+                    <p>Alumni Management System @ 2020</p>
                 </div>
             </div>
         </div>
